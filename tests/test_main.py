04f1c97790f032edf64f364936eda79b112852f1
tzschmidt <tzschmidt@gmx.de>
2024-04-18 12:46:00 +0200

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,10 +4,9 @@ Test cases for main application functionality.
 
 # pylint: disable=protected-access, too-many-public-methods, duplicate-code
 import logging
-import os
 import random
 from io import StringIO
-from unittest import TestCase, mock
+from unittest import TestCase
 
 import clingo
 from clingo.symbol import Function, Number
@@ -16,12 +15,6 @@ from large_neighbourhood_search import LNS
 from large_neighbourhood_search.lib import lns_functions as lns_f
 from large_neighbourhood_search.utils.logger import setup_logger
 from large_neighbourhood_search.utils.parser import get_parser
-from large_neighbourhood_search.utils.pf_handling import (
-    create_param_file,
-    gen_example_params,
-    load_param_file,
-    save_param_file,
-)
 
 
 class TestMain(TestCase):
@@ -47,103 +40,6 @@ class TestMain(TestCase):
         parser = get_parser()
         ret = parser.parse_args(["--log", "info"])
         self.assertEqual(ret.log, logging.INFO)
-
-    def test_load_param_file(self):
-        """
-        Test the loading of parameters from file.
-        """
-        ref_content = {
-            "name": "example_params",
-            "relaxation": {
-                "mode": "declarative",
-                "rates": [0.2, 0.4, 0.6],
-                "threshold": 3,
-            },
-            "search": {
-                "mode": "hard_constraint",
-                "bound": {"mode": "overall", "type": "steps", "value": 2000},
-            },
-            "seed": None,
-        }
-        test_content = load_param_file("./tests/ref/example_params_ref.json")
-        self.assertDictEqual(test_content, ref_content)
-
-    def test_save_param_file(self):
-        """
-        Test the saving of parameters.
-        """
-        ref_content = {
-            "name": "example_params",
-            "relaxation": {
-                "mode": "declarative",
-                "rates": [0.2, 0.4, 0.6],
-                "threshold": 3,
-            },
-            "search": {
-                "mode": "hard_constraint",
-                "bound": {"mode": "overall", "type": "steps", "value": 2000},
-            },
-            "seed": None,
-        }
-        save_param_file(ref_content, "./tests/test.json")
-        test_content = load_param_file("./tests/test.json")
-        os.remove("./tests/test.json")
-        self.assertDictEqual(test_content, ref_content)
-
-    def test_gen_example_param_file(self):
-        """
-        Test the generation of an example parameter file.
-        """
-        gen_example_params("./tests")
-        test_content = load_param_file("./tests/example_params.json")
-        os.remove("./tests/example_params.json")
-        ref_content = load_param_file("./tests/ref/example_params_ref.json")
-        self.assertDictEqual(test_content, ref_content)
-
-    @mock.patch("large_neighbourhood_search.utils.pf_handling.input", create=True)
-    def test_create_param_file(self, mocked_input):
-        """
-        Test the creation of a new parameter file.
-        """
-        mocked_input.side_effect = [
-            "test",
-            "bla",
-            "1",
-            "-2",
-            "0.2",
-            "x",
-            "0.4",
-            "0",
-            "-3",
-            "3",
-            "3",
-            "0",
-            "-2",
-            "1",
-            "bla",
-            "0",
-            "-3",
-            "1500",
-            "bla",
-            "None",
-        ]
-        create_param_file("./tests/")
-        test_content = load_param_file("./tests/test.json")
-        os.remove("./tests/test.json")
-        ref_content = {
-            "name": "test",
-            "relaxation": {
-                "mode": "declarative",
-                "rates": [0.2, 0.4],
-                "threshold": 3,
-            },
-            "search": {
-                "mode": "hard_constraint",
-                "bound": {"mode": "per_improvement", "type": "steps", "value": 1500},
-            },
-            "seed": None,
-        }
-        self.assertDictEqual(test_content, ref_content)
 
     def test_lns_init(self):
         """
