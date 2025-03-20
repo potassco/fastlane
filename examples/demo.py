9a28c1941e2691111f2c8d0de564127969700d21
Tom Schmidt <tom.schmidt@uni-potsdam.de>
2025-03-20 10:18:15 +0100

    Update demo

--- examples/demo.py
+++ examples/demo.py
@@ -4,99 +4,80 @@ Examples on how to use the LNS framework.
 Check out the Guide section in the documentation for
 a step by step introduction.
 """
-from typing import Any, Dict
-
 from mod_lns import LNS
-from mod_lns.lib.solvers.clingo_solver import ClingoSolver
-from mod_lns.lib.relaxation import relax_declarative
+from mod_lns.lns_config import LNSConfig
 from mod_lns.utils.conversions import symbol_to_str
 
-# classic LNS with random relaxation using clingo with assumptions and weighted sum
-def config1():
-    solver = ClingoSolver()
-    strategy = ClassicWeightedSumRnd()
-    params: Dict[str, Any] = {
-        "seed": 123,
-        "relax_rate": 0.2,
-    }
-    return solver, strategy, params
-
-def config1_1():
-    solver = ClingoSolver()
-    strategy = ClassicWeightedSumRnd()
-    params: Dict[str, Any] = {
-        "seed": 123,
-        "relax_rate": 0.4,
-    }
-    return solver, strategy, params
-
-# LNS using clingo with assumptions and hard constraints with random relaxation and weighted sum
-def config2():
-    solver = ClingoSolver()
-    strategy = HCWeightedSumRnd()
-    params: Dict[str, Any] = {
-            "seed": 123,
-            "solve_time_limit": 10,
-        }
-    return solver, strategy, params
-
-# classic LNS with random relaxation using clingo with heuristics and weighted sum
-def config3():
-    solver = ClingoHeuSolver()
-    strategy = ClassicWeightedSumRnd()
-    params: Dict[str, Any] = {
-        "seed": 123,
-        "stuck_after_no_improv": 300,
-    }
-    return solver, strategy, params
-
-# LNS using clingo with heuristics and hard constraints with random relaxation and weighted sum
-def config3_1():
-    solver = ClingoHeuSolver()
-    strategy = HCWeightedSumRnd()
-    params: Dict[str, Any] = {
-        "seed": 123,
-        "solve_time_limit": 10,
-        "stuck_after_no_improv": 300,
-    }
-    return solver, strategy, params
-
-# Implementation of classic LNS with declarative relaxation and weighted sum
-class ClassicWeightedSumDecl(ClassicWeightedSumRnd):
-    def relax(self, model, relax_parameters):
-        r = relax_declarative(model, relax_parameters)
-        #for s in r:
-        #    print(symbol_to_str(s[0]))
-        #print("--")
-        return r
-
-# classic LNS with declarative relaxation using clingo with assumptions and weighted sum
-def config4():
-    solver = ClingoSolver()
-    strategy = ClassicWeightedSumDecl()
-    params: Dict[str, Any] = {
-        "seed": 123,
-    }
-    return solver, strategy, params
-
-def config4_1():
-    solver = ClingoSolver()
-    strategy = ClassicWeightedSumDecl()
-    params: Dict[str, Any] = {
-        "seed": 123,
-        "max_steps": 8,
-    }
-    return solver, strategy, params
-
-
-from mod_lns.lns_config import LNSConfig
-
 def main():
-    #solver, strategy, params = config1()
-    #lns = LNS(["examples/golf_demo.lp"], solver, strategy, params)
-    #lns.main()
-    config = LNSConfig(lns_options={"hc":True,"decl":False, "heu":False})
-    lns = LNS(["examples/golf_demo.lp"], config)
+    # classic LNS with random relaxation using clingo with assumptions
+    cl_config = LNSConfig(lns_options={"seed": 123})
+    cl2_config = LNSConfig(
+        lns_options={
+            "seed": 123, 
+            "relax_rate": 0.4
+        })
+
+    # constrained LNS using clingo with assumptions and random relaxation
+    hc_config = LNSConfig(
+        lns_options={
+            "constrained":True, 
+            "seed": 123, 
+            "solve_time_limit": 10
+        })
+
+    # classic LNS with declarative relaxation using clingo with assumptions
+    cl_decl_config = LNSConfig(
+        lns_options={
+            "declarative": True, 
+            "seed": 123
+        })
+    
+    # Example of custom configuration
+    class NewLNSConfig(LNSConfig):
+        
+        def __init__(self, lns_options = {}, clingo_options = []):
+            # add new default value
+            default_options = {
+                "new_opt": False,
+            }
+            self.lns_options = {**default_options, **lns_options}
+
+            # keep functionality of LNSConfig
+            super().__init__(self.lns_options, clingo_options)
+            
+            # add new functionality
+            if self.lns_options["new_opt"] == True:
+                self._enable_new_opt()
+        
+        def _enable_new_opt(self):
+            # get current strategy to modify
+            base = type(self.strategy)
+            class EnNewOpt(base):
+                def relax(
+                    self,
+                    model,
+                    relax_parameters,
+                ):
+                    # keep functionality
+                    r = super(EnNewOpt, self).relax(model,relax_parameters)
+                    # new functionality
+                    for s in r:
+                        print(symbol_to_str(s[0]))
+                    print("--")
+                    return r
+            # set new strategy
+            self.strategy = EnNewOpt()
+
+    # Use new config to inspect declarative relaxation
+    cl_decl_custom_config = NewLNSConfig(
+        lns_options={
+            "new_opt": True, 
+            "declarative": True, 
+            "seed": 123, 
+            "max_steps": 5
+        })
+
+    lns = LNS(["examples/golf_demo.lp"], cl_config)
     lns.main()
 
 if __name__ == "__main__":
