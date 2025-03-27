e18977e1d852b3a1e7bad3f891a7c5541e515e27
tzschmidt <tzschmidt@gmx.de>
2025-03-27 14:56:55 +0100

    Update demo

--- examples/demo.py
+++ examples/demo.py
@@ -4,7 +4,7 @@ Examples on how to use the LNS framework.
 Check out the Guide section in the documentation for
 a step by step introduction.
 """
-from mod_lns import LNS
+from mod_lns.lns import LNS
 from mod_lns.lns_config import LNSConfig
 from mod_lns.utils.conversions import symbol_to_str
 
