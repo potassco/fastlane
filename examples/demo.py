7d7a65cb675bc0c4731fae40f1cf9b9d34c383a0
tzschmidt <tzschmidt@gmx.de>
2025-09-26 14:53:06 +0200

    Update examples

--- examples/demo.py
+++ examples/demo.py
@@ -5,77 +5,45 @@ Check out the Guide section in the documentation for
 a step by step introduction.
 """
 from mod_lns.lns import LNS
-from mod_lns.lns_config import LNSConfig
+from mod_lns.lib.strategies.default_strategy import DefaultStrategy, LNSConfig
+from mod_lns.lib.strategies.heulingo import Heulingo, HeulingoConfig
 from mod_lns.utils.conversions import symbol_to_str
 
 
 def main():
     # classic LNS with random relaxation using clingo with assumptions
-    cl_config = LNSConfig(
-        lns_options={
-            "seed": 123,
-            "relax_rate": 0.2,
-        }
-    )
+    cl_strategy = DefaultStrategy()
+    cl_strategy.config = LNSConfig(seed=123, relax_rate=40, init_time_limit=2, time_limit=None, max_steps=500)
 
     # constrained LNS using clingo with assumptions and random relaxation
-    con_config = LNSConfig(
-        lns_options={
-            "constrained": True,
-            "seed": 123,
-            "relax_rate": 0.2,
-            "solve_time_limit": 10,
-        }
-    )
+    con_strategy = DefaultStrategy()
+    con_strategy.config = LNSConfig(seed=123, relax_rate=40, init_time_limit=2, time_limit=20, max_steps=500, constrained=True)
 
     # classic LNS with declarative relaxation using clingo with assumptions
-    cl_decl_config = LNSConfig(lns_options={"declarative": True, "seed": 123})
+    cl_decl_strategy = DefaultStrategy()
+    cl_decl_strategy.config = LNSConfig(seed=123, relax_rate=40, init_time_limit=2, max_steps=500, declarative=True)
 
-    # Example of custom configuration
-    class NewLNSConfig(LNSConfig):
-        def __init__(self, lns_options={}, clingo_options=[]):
-            # add new default value
-            default_options = {
-                "new_opt": False,
-            }
-            self.lns_options = {**default_options, **lns_options}
-
-            # keep functionality of LNSConfig
-            super().__init__(self.lns_options, clingo_options)
-
-            # add new functionality
-            if self.lns_options["new_opt"] == True:
-                self._enable_new_opt()
-
-        def _enable_new_opt(self):
-            # get current strategy to modify
-
-            def relax(
-                self,
-                model,
-                relax_parameters,
-            ):
-                # keep functionality
-                r = super(EnNewOpt, self).relax(model, relax_parameters)
-                # new functionality
-                for s in r:
-                    print(symbol_to_str(s[0]))
-                print("--")
-                return r
-
-            # set new strategy
-            base = type(self.strategy)
-            EnNewOpt = type("EnNewOpt", (base,), {"relax": relax})
-            self.strategy = EnNewOpt()
+    # Example of custom strategy
+    class NewStrategy(DefaultStrategy):
+        def relax(self, lns_object):
+            r = super().relax(lns_object)
+            for s in r:
+                print(symbol_to_str(s))
+            print("--")
+            return r
 
     # Use new config to inspect declarative relaxation
-    con_decl_custom_config = NewLNSConfig(
-        lns_options={"new_opt": True, "declarative": True, "seed": 123, "max_steps": 5}
-    )
+    decl_custom = NewStrategy()
+    decl_custom.config = LNSConfig(seed=123, relax_rate=20, init_time_limit=2, max_steps=5, declarative=True)
 
-    lns = LNS(["examples/golf_demo.lp"], cl_config)
+    lns = LNS(["examples/golf_demo.lp"], cl_strategy)
+
+    # Example using heulingo strategy
+    heuristic_strategy = Heulingo()
+    heuristic_strategy.config = HeulingoConfig(seed=123, init_time_limit=2, lns_time_limit=2, max_steps=500, clingo_args="-c n=40")
+    #lns = LNS(["examples/golf_demo.lp", "examples/golf_lnps.lp"], heuristic_strategy)
+
     lns.main()
-
 
 if __name__ == "__main__":
     main()
