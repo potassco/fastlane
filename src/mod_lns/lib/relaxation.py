1c8ef00834a255bb48a7ece62ed2a57515ea5696
tzschmidt <tzschmidt@gmx.de>
2026-05-18 15:16:41 +0200

    Use sets for relaxation

--- src/mod_lns/lib/relaxation.py
+++ src/mod_lns/lib/relaxation.py
@@ -13,7 +13,7 @@ from mod_lns import Model
 def relax_declarative(
     model: Model,
     relax_rate: int,
-) -> list[Symbol]:
+) -> set[Symbol]:
     """
     Relax portion of selected atoms given by the relax_rate.
     ASP encoding has to contain `_lns_select/1` and `_lns_fix/2` predicates.
@@ -23,22 +23,22 @@ def relax_declarative(
     :param relax_rate: Percentage of atoms to relax.
     :type relax_rate: int
     :return: Fixed (not relaxed) atoms.
-    :rtype: list[Symbol]
+    :rtype: set[Symbol]
     """
-    fixed_atoms: list[Symbol] = []
-    selected_atoms: list[Symbol] = []
-    declared_fixed_atoms: dict[clingo.symbol.Symbol, list[Symbol]] = {}
+    fixed_atoms: set[Symbol] = set()
+    selected_atoms: set[Symbol] = set()
+    declared_fixed_atoms: dict[clingo.symbol.Symbol, set[Symbol]] = {}
     # !inefficient
     for atom in model.true:
         # get possible selection
         if atom.match("_lns_select", 1):
             if atom.arguments[0] not in selected_atoms:
-                selected_atoms.append(atom.arguments[0])
-                declared_fixed_atoms[atom.arguments[0]] = []
+                selected_atoms.add(atom.arguments[0])
+                declared_fixed_atoms[atom.arguments[0]] = set()
         # associate selecttion with fixed atoms
         elif atom.match("_lns_fix", 2):
             if atom.arguments[1] in declared_fixed_atoms:
-                declared_fixed_atoms[atom.arguments[1]].append(atom.arguments[0])
+                declared_fixed_atoms[atom.arguments[1]].add(atom.arguments[0])
     # sample selection atoms
     if len(selected_atoms) == 1:
         symbols = selected_atoms.copy()
@@ -49,14 +49,14 @@ def relax_declarative(
         )
     # fix corresponding atoms
     for s in symbols:
-        fixed_atoms += declared_fixed_atoms[s]
+        fixed_atoms.update(declared_fixed_atoms[s])
     return fixed_atoms
 
 
 def relax_random(
     model: Model,
     relax_rate: int,
-) -> list[Symbol]:
+) -> set[Symbol]:
     """
     Relax random number of shown atoms given by the relax_rate.
 
@@ -65,7 +65,7 @@ def relax_random(
     :param relax_rate: Percentage of atoms to relax.
     :type relax_rate: int
     :return: Fixed (not relaxed) atoms.
-    :rtype: list[Symbol]
+    :rtype: set[Symbol]
     """
-    fixed_atoms = random.sample(model.shown, round(len(model.shown) * (1 - relax_rate / 100)))
-    return fixed_atoms
+    fixed_atoms = random.sample(sorted(model.shown), round(len(model.shown) * (1 - relax_rate / 100)))
+    return set(fixed_atoms)
