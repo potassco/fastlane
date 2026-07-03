aa9975ff3ea34ddc1acd5d9d5e61ac4edca8509b
tzschmidt <tzschmidt@gmx.de>
2026-07-03 14:01:32 +0200

    Add solver tests

---
+++ tests/solver/test_clingcon.py
@@ -0,0 +1,35 @@
+"""
+Test cases for ClingconSolver class.
+"""
+
+import clingcon
+import clingo
+
+from mod_lns.lib.solvers.clingcon_solver import ClingconSolver
+from mod_lns.lns import LNS
+from tests.solver.test_clingo import TestSolverClingo
+
+
+class TestClingconSolver(TestSolverClingo):
+    """
+    Test cases for ClingconSolver class.
+    """
+
+    def setUp(self) -> None:
+        self.solver = ClingconSolver()
+        self.stype = ClingconSolver
+        self.name = "clingcon"
+        self.lns = LNS(["./tests/ref/golf.lp"], {"log_level": 50})
+
+    def test_setup(self):
+        """
+        Test clingcon setup.
+        """
+        self.solver.setup(self.lns)
+        self.assertIsInstance(self.solver.control, clingo.control.Control)
+        self.assertIsInstance(self.solver.theory, clingcon.ClingconTheory)
+
+        self.solver.setup(self.lns, ["--models=2"], ["./tests/ref/golf.lp"])
+        self.assertIsInstance(self.solver.control, clingo.control.Control)
+        self.assertIsInstance(self.solver.theory, clingcon.ClingconTheory)
+        self.assertEqual(self.solver.control.configuration.solve.models, "2")
